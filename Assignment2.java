313577a7fe7b04bb7b3b00117398ec27f5f40036
Tanjeem_817 <pbpb4709@gmail.com>
2026-09-18 04:01:31 +0600

    Just Print Out

--- Assignment2.java
+++ Assignment2.java
@@ -7,3 +7,19 @@
 101,iphone15,1895 euros,perfect product with best image quality, phone,
 * step 5: print the data
  */
+package com.mycompany.product;
+public class Product{
+    public static void main(String[] args){
+        int id = 101;
+        String title = "iphone15";
+        String price = "1895 euros";
+        String description = "perfect with best image quality";
+        String category = "phone";
+        System.out.println("ID\t\t:"+id);
+        System.out.println("Title\t\t:"+title);
+        System.out.println("Price\t\t:"+price);
+        System.out.println("Description\t:"+description);
+        System.out.println("Category\t:"+category);
+    
+    }
+}
