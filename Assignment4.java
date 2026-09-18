44b66f8555fc073f62831116392c2fa8cf58fd17
Tanjeem_817 <pbpb4709@gmail.com>
2026-09-18 19:45:10 +0600

    Refactor Assignment4 for installment calculation
    Refactor installment calculation program and update package structure.

--- Assignment4.java
+++ Assignment4.java
@@ -1,21 +1,35 @@
-import java.util.Scanner;
-// Create a program to calculate installment amount for per month
-public class Assignment4 {
-  public static void main(String[] args) {
+// import java.util.Scanner;
+// // Create a program to calculate installment amount for per month
+// public class Assignment4 {
+//   public static void main(String[] args) {
 
-    try (Scanner input = new Scanner(System.in)) {
-      int phonePrice = 1800; // 1800 euros
-      int numberOfInstallment, installmentPerMonth;
+//     try (Scanner input = new Scanner(System.in)) {
+//       int phonePrice = 1800; // 1800 euros
+//       int numberOfInstallment, installmentPerMonth;
 
-      System.out.print("Number of installments? ");
-      // get number of installments from user
+//       System.out.print("Number of installments? ");
+//       // get number of installments from user
      
 
-      // calculate  installment amount for per month
+//       // calculate  installment amount for per month
     
 
-        System.out.println("Monthly installment Amount: "+installmentPerMonth + " euros");
-    } 
+//         System.out.println("Monthly installment Amount: "+installmentPerMonth + " euros");
+//     } 
 
-  }
+//   }
+// }
+package main.product;
+import java.util.Scanner;
+public class Assignment4{
+    public static void main(String[] args){
+     Scanner input = new Scanner(System.in);
+     int phonePrice = 1800;
+     int numberofInstallment;
+     System.out.print("Number of installments? :");
+     numberofInstallment = input.nextInt();
+     double installmentPermonth = (double)phonePrice / numberofInstallment;
+     System.out.println(installmentPermonth);
+     
+    }
 }
