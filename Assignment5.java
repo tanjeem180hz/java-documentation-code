655a9f0031d2c1798f365918b6ba5fd88a75a833
Tanjeem_817 <pbpb4709@gmail.com>
2026-09-18 20:32:47 +0600

    Update Assignment5.java

--- Assignment5.java
+++ Assignment5.java
@@ -1,11 +1,26 @@
-/*
- * valid voter program using if,else 
- * step 1: ask for a person age
- * step 2: if age is equal or more than 18 than print valid voter
- * step 3: else print invalid voter
- */
-public class Assignment5 {
-  public static void main(String[] args) {
+// /*
+//  * valid voter program using if,else 
+//  * step 1: ask for a person age
+//  * step 2: if age is equal or more than 18 than print valid voter
+//  * step 3: else print invalid voter
+//  */
+// public class Assignment5 {
+//   public static void main(String[] args) {
     
-  }
+//   }
+// }
+package main.product;
+import java.util.Scanner;
+public class voter{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        int age = input.nextInt();
+        if(age>=18){
+            System.out.println("Valid Voter");
+        }
+        else{
+            System.out.println("Invalid Voter");
+        }
+    }
 }
